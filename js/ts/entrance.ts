487bdc6db3b81fae36d1051d568caa5877e6b31c
luzac3 <rey.silvy@gmail.com>
2021-07-11 11:59:20 +0900

    アクセストークンの処理を一部修正

--- js/ts/entrance.ts
+++ js/ts/entrance.ts
@@ -1,5 +1,11 @@
+import { HandleCookie } from "./handleCookie";
+
 $(document).ready(() => {
+  // Cookieに保存されているアクセストークンを削除
+  const handleCookie = new HandleCookie();
+  handleCookie.delCookie('accessToken');
+
     $(".enter_button").on("click",() => {
-        location.href="./main.html";
+        //location.href = "./oAuth.html";
     });
 });
